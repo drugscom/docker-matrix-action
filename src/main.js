e8f13f5bb8300a2f7f734cdc516a502707e7d032
Andre Sencioles <asenci@gmail.com>
2021-03-18 14:50:54 +1300

    Fix version tagging support

--- src/main.js
+++ src/main.js
@@ -4,71 +4,82 @@ const path = require('path');
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const semver = require('semver');
+
+const gitRefRegex = /^refs\/(:?heads|tags)\//;
+
+function getTags(gitRef) {
+    const latestBranch = core.getInput('latest-branch');
+    const latestRegex = new RegExp(`^refs/heads/${latestBranch}`);
+
+    if (gitRef.match(latestRegex)) {
+        return ['latest'];
+    }
+
+    let gitRefClean = gitRef.replace(gitRefRegex, '');
+
+    if (gitRef.match(/^refs\/tags\//)) {
+        let tagVersion = semver.parse(gitRefClean, {loose: true, includePrerelease: true});
+        if (tagVersion) {
+            if (tagVersion.prerelease.length) {
+                return [tagVersion.version];
+            }
+
+            return [
+                `${tagVersion.major}.${tagVersion.minor}.${tagVersion.patch}`,
+                `${tagVersion.major}.${tagVersion.minor}`,
+                `${tagVersion.major}`,
+            ];
+        }
+    }
+
+    return [gitRefClean];
+}
+
 
 try {
     const paths = core.getInput('paths');
-    const latestBranch = core.getInput('latest-branch');
     const recursive = core.getInput('recursive');
     const tagPrefixInput = core.getInput('tag-prefix');
 
     includes = [];
     jobMatrix = {'include': includes};
 
+    if (!github.context.ref.match(gitRefRegex)) {
+        core.setFailed(`Received "${github.context.eventName}" event with invalid ref: ${github.context.ref}` );
+    }
+
     core.startGroup('Find targets');
     for (let line of paths.split('\n')) {
         for (let dir of line.split(',')) {
             let globPath = path.join(dir, 'Dockerfile');
-            if (recursive) {
+            if (recursive === 'true') {
                 globPath = path.join(dir, '**/Dockerfile');
             }
 
             for (let dockerFile of glob.sync(globPath)) {
                 if (fs.lstatSync(dockerFile).isDirectory()) {
-                    core.info('Ignoring ' + dockerFile + ' (is a directory)');
+                    core.warning(`Ignoring directory "${dockerFile}"`);
                     continue;
                 }
-                core.info('Found ' + dockerFile);
 
                 let dockerFilePath = path.dirname(dockerFile);
+                core.info(`Found Dockerfile on "${dockerFilePath}"`);
 
                 let cachePath = path.join(github.context.repo.owner, github.context.repo.repo, dockerFilePath);
-                let tags = [];
+
+                let tags = getTags(github.context.ref);
+
                 let tagPrefix = ['ghcr.io', github.context.repo.owner, github.context.repo.repo].join('/');
                 if (tagPrefixInput !== '') {
                     tagPrefix = tagPrefixInput;
                 }
-                let tagSuffix = path.relative(dir, dockerFilePath).replace(path.sep, '-');
 
-                if (github.context.eventName === 'push') {
-                    let branchName = github.context.ref.replace(/^refs\/heads\//, '');
-                    branchName = branchName.replace('/', '-');
-                    branchName = branchName.replace(/[^-a-zA-Z0-9_.]/, '');
-
-                    if (branchName === latestBranch) {
-                        tags = ['latest'];
-                    } else {
-                        tags = [branchName];
-                    }
-                } else if (github.context.eventName === 'label') {
-                    let label = github.context.ref.replace(/^refs\/tags\//, '');
-
-                    if (label.match(/^v?[0-9]+(?:\.[0-9]+(?:\.[0-9]+)?)?(?:-?(?:b|beta)[0-9]+)?$/)) {
-                        label = label.replace(/^v/, '');
-
-                        if (!label.match(/(?:b|beta)[0-9]+$/)) {
-                            while (label.match(/\./)) {
-                                tags.push(label);
-                                label = label.replace(/\.[^.]*$/, '');
-                            }
-                        }
-                    }
-
-                    tags.push(label);
-                }
-
+                let tagSuffix = path.relative(dir, dockerFilePath);
                 if (tagSuffix) {
                     tags = tags.map((tag) => [tag, tagSuffix].join('-'));
                 }
+
                 tags = tags.map((tag) => tag.replace('/', '-'));
                 tags = tags.map((tag) => tag.replace(/[^-a-zA-Z0-9_.]/, ''));
                 tags = tags.map((tag) => [tagPrefix, tag].join(':').substr(0, 128));
